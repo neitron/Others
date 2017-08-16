07ec59f200e0e20d23be338391313dbfe743fe4e
Kirill Zubenko <kirill.zubenko.neitron@gmail.com>
2017-08-17 01:40:25 +0300

    First prototype base - player controller + navMesh + camera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    private float currentZoom = 10;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    const int RMB = 1;
+
+    public LayerMask layerMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100.0f, layerMask))
+                {
+                    motor.MoveToPoint(hit.point);
+                }
+            }
+        }
+        if(Input.GetMouseButtonDown(RMB))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100.0f, layerMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+	}
+}
