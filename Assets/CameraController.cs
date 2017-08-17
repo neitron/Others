6c894c20e29e641067bfcf6c54d28f11641423cf
Kirill Zubenko <kirill.zubenko.neitron@gmail.com>
2017-08-17 18:10:13 +0300

    Camera changes

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset;
+    public Transform lookAtTarget;
 
-    private float currentZoom = 10;
+    public float up;
+
+    public float currentZoom = 10;
+
+    public float observeAngle = 135;
 
     private void LateUpdate()
     {
-        transform.position = target.position - offset * currentZoom;
+        var tp = target.position;
+        tp.y = 0.0f;
+
+        var lp = lookAtTarget.position;
+        lp.y = 0.0f;
+
+        var cp = transform.position;
+        cp.y = 0.0f;
+
+        var playerToTowerDir = (tp - lp).normalized;
+        var towerToCamDir = (cp - lp).normalized;
+
+        var res = ( Vector3.Dot(-playerToTowerDir, towerToCamDir) * 0.5f + 0.5f ) * 180;
+        
+        if (res > observeAngle)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position + playerToTowerDir * currentZoom + Vector3.up * up, 0.01f);
+            transform.LookAt(lp);
+        }
+         
+
     }
 }
